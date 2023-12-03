3ccc0c29fb2ef89e5b5d61b20d5cbb2db5dc38f7
ASUS <ASUS@SUPERAMAN>
2023-12-03 23:04:12 +0530

    Add project files.

---
+++ enc_temp_folder/f1ffe61313ec9a443d4b0741b79a498/test.cpp
@@ -0,0 +1,27 @@
+#include<opencv2/imgcodecs.hpp>
+#include<opencv2/highgui.hpp>
+#include<opencv2/imgproc.hpp>
+#include<opencv2/objdetect.hpp>
+#include<iostream>
+
+using namespace std;
+using namespace cv;
+
+/*int main() {
+	string path = "D:/users/ASUS/Codes/openCV Project with C++/opencv/build/x64/vc16/lib/download.jpg";
+	Mat img = imread(path);
+	imshow("Frame", img);
+	waitKey(0);
+	return 0;
+}*/
+
+int main() {
+	VideoCapture video(1);
+	Mat img;
+	while (true) {
+		video.read(img);
+		imshow("Frame", img);
+		waitKey(1);
+	}
+	return 0;
+}
