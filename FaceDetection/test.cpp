279fbefc59ac9c74105f1832879ab57f56e11017
Aman Vishwakarma <123929292+superAman07@users.noreply.github.com>
2023-12-04 13:06:29 +0530

    test.cpp

--- FaceDetection/test.cpp
+++ FaceDetection/test.cpp
@@ -16,11 +16,26 @@ using namespace cv;
 }*/
 
 void main() {
-	VideoCapture video(0);
+	VideoCapture video(0);// we can put any video by its path...
+	//VideoCapture video("C:/Users/ASUS/Pictures/Camera Roll/WIN_20230621_07_38_51_Pro.mp4");
 	Mat img;
-	while (true) {
 
+	CascadeClassifier facedetect;
+	
+	facedetect.load("haarcascade_frontalface_default.xml");
+	
+	while (true) {
 		video.read(img);
+
+		vector<Rect> faces;
+		facedetect.detectMultiScale(img, faces, 1.3, 5);
+		cout << faces.size() << "\n";
+		for (int i = 0; i < faces.size(); i++) {
+			rectangle(img, faces[i].tl(), faces[i].br(), Scalar(50, 50, 255),3);
+			rectangle(img, Point(0,0), Point(200,35), Scalar(50, 50, 255), 100);
+			putText(img, to_string(faces.size())+ " Face Found", Point(10, 40), FONT_HERSHEY_DUPLEX, 1, Scalar(255, 255, 255), 1);
+		}
+
 		imshow("Frame", img);
 		waitKey(1);
 
